32648907cec4b44635d91b54c75cae6d46324348
NamTang <namkty96@gmail.com>
2018-08-30 01:05:17 +0700

    add: certificates article and images, change: delete unused function and custom prev and next button click event

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,6 @@
 2. Set Header
 3. Init Home Slider
 4. Init Menu
-5. Init Search
-6. Init CTA Slider
-7. Init Testimonials Slider
-8. Init Search Form
-
 
 ******************************/
 $(document).ready(function () {
@@ -28,11 +23,8 @@ $(document).ready(function () {
 	var menuActive = false;
 	var sideMenu = $('.hamburger-side-menu');
 	var header = $('.header');
-	var searchActive = false;
-
-	/*
-	setHeader();
-	*/
+	var imgIndex = 0;
+	var images = ["./images/cert-java.jpg", "./images/cert-html.jpg", "./images/cert-css.jpg", "./images/cert-js.jpg", "./images/cert-jquery.jpg", "./images/cert-php.jpg", "./images/cert-sql.jpg"];
 
 	$('a[href^="#"]').on('click', function (event) {
 		event.preventDefault();
@@ -57,19 +49,19 @@ $(document).ready(function () {
 			sideMenu.removeClass('d-none');
 		} else if (window.innerWidth <= 575 && window.innerWidth > 377 && window.scrollY >= 60) {
 			sideMenu.removeClass('d-none');
+		} else if (window.innerWidth <= 377 && window.scrollY >= 80) {
+			sideMenu.removeClass('d-none');
 		} else {
 			sideMenu.addClass('d-none');
 		}
 	});
 
-	initMenu();
 	/*
-	initHomeSlider();
-	initSearch();
-	initCtaSlider();
-	initTestSlider();
-	initSearchForm();
+	setHeader();
 	*/
+	initMenu();
+	initHomeSlider();
+
 	/* 
 
 	2. Set Header
@@ -105,80 +97,34 @@ $(document).ready(function () {
 	*/
 
 	function initHomeSlider() {
-		if ($('.home_slider').length) {
-			var homeSlider = $('.home_slider');
 
-			homeSlider.owlCarousel(
-				{
-					items: 1,
-					loop: true,
-					autoplay: false,
-					smartSpeed: 1200,
-					dotsContainer: 'main_slider_custom_dots'
-				});
+		/* Custom nav events */
+		if ($('.home_slider_prev').length) {
 
-			/* Custom nav events */
-			if ($('.home_slider_prev').length) {
-				var prev = $('.home_slider_prev');
+			$('.home_slider_prev').on('click', function () {
+				imgIndex -= 1;
 
-				prev.on('click', function () {
-					homeSlider.trigger('prev.owl.carousel');
-				});
-			}
+				if (imgIndex < 0) {
+					imgIndex = images.length - 1;
+				}
 
-			if ($('.home_slider_next').length) {
-				var next = $('.home_slider_next');
-
-				next.on('click', function () {
-					homeSlider.trigger('next.owl.carousel');
-				});
-			}
-
-			/* Custom dots events */
-			if ($('.home_slider_custom_dot').length) {
-				$('.home_slider_custom_dot').on('click', function () {
-					$('.home_slider_custom_dot').removeClass('active');
-					$(this).addClass('active');
-					homeSlider.trigger('to.owl.carousel', [$(this).index(), 300]);
-				});
-			}
-
-			/* Change active class for dots when slide changes by nav or touch */
-			homeSlider.on('changed.owl.carousel', function (event) {
-				$('.home_slider_custom_dot').removeClass('active');
-				$('.home_slider_custom_dots li').eq(event.page.index).addClass('active');
+				$('img.center-fit').attr('src', images[imgIndex]);
 			});
-
-			// add animate.css class(es) to the elements to be animated
-			function setAnimation(_elem, _InOut) {
-				// Store all animationend event name in a string.
-				// cf animate.css documentation
-				var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-				_elem.each(function () {
-					var $elem = $(this);
-					var $animationType = 'animated ' + $elem.data('animation-' + _InOut);
-
-					$elem.addClass($animationType).one(animationEndEvent, function () {
-						$elem.removeClass($animationType); // remove animate.css Class at the end of the animations
-					});
-				});
-			}
-
-			// Fired before current slide change
-			homeSlider.on('change.owl.carousel', function (event) {
-				var $currentItem = $('.home_slider_item', homeSlider).eq(event.item.index);
-				var $elemsToanim = $currentItem.find("[data-animation-out]");
-				setAnimation($elemsToanim, 'out');
-			});
-
-			// Fired after current slide has been changed
-			homeSlider.on('changed.owl.carousel', function (event) {
-				var $currentItem = $('.home_slider_item', homeSlider).eq(event.item.index);
-				var $elemsToanim = $currentItem.find("[data-animation-in]");
-				setAnimation($elemsToanim, 'in');
-			})
 		}
+
+		if ($('.home_slider_next').length) {
+
+			$('.home_slider_next').on('click', function () {
+				imgIndex += 1;
+
+				if (imgIndex >= images.length) {
+					imgIndex = 0;
+				}
+
+				$('img.center-fit').attr('src', images[imgIndex]);
+			});
+		}
+
 	}
 
 	/* 
@@ -233,146 +179,5 @@ $(document).ready(function () {
 		header.removeClass('position-fixed');
 		sideMenu.removeClass('d-none');
 		menuActive = false;
-	}
-
-	/* 
-
-	5. Init Search
-
-	*/
-
-	function initSearch() {
-		if ($('.search_tab').length) {
-			$('.search_tab').on('click', function () {
-				$('.search_tab').removeClass('active');
-				$(this).addClass('active');
-				var clickedIndex = $('.search_tab').index(this);
-
-				var panels = $('.search_panel');
-				panels.removeClass('active');
-				$(panels[clickedIndex]).addClass('active');
-			});
-		}
-	}
-
-	/* 
-
-	6. Init CTA Slider
-
-	*/
-
-	function initCtaSlider() {
-		if ($('.cta_slider').length) {
-			var ctaSlider = $('.cta_slider');
-
-			ctaSlider.owlCarousel(
-				{
-					items: 1,
-					loop: true,
-					autoplay: false,
-					nav: false,
-					dots: false,
-					smartSpeed: 1200
-				});
-
-			/* Custom nav events */
-			if ($('.cta_slider_prev').length) {
-				var prev = $('.cta_slider_prev');
-
-				prev.on('click', function () {
-					ctaSlider.trigger('prev.owl.carousel');
-				});
-			}
-
-			if ($('.cta_slider_next').length) {
-				var next = $('.cta_slider_next');
-
-				next.on('click', function () {
-					ctaSlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-	}
-
-	/* 
-
-	7. Init Testimonials Slider
-
-	*/
-
-	function initTestSlider() {
-		if ($('.test_slider').length) {
-			var testSlider = $('.test_slider');
-
-			testSlider.owlCarousel(
-				{
-					loop: true,
-					nav: false,
-					dots: false,
-					smartSpeed: 1200,
-					margin: 30,
-					responsive:
-					{
-						0: { items: 1 },
-						480: { items: 1 },
-						768: { items: 2 },
-						992: { items: 3 }
-					}
-				});
-
-			/* Custom nav events */
-			if ($('.test_slider_prev').length) {
-				var prev = $('.test_slider_prev');
-
-				prev.on('click', function () {
-					testSlider.trigger('prev.owl.carousel');
-				});
-			}
-
-			if ($('.test_slider_next').length) {
-				var next = $('.test_slider_next');
-
-				next.on('click', function () {
-					testSlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-	}
-
-	/* 
-
-	8. Init Search Form
-
-	*/
-
-	function initSearchForm() {
-		if ($('.search_form').length) {
-			var searchForm = $('.search_form');
-			var searchInput = $('.search_content_input');
-			var searchButton = $('.content_search');
-
-			searchButton.on('click', function (event) {
-				event.stopPropagation();
-
-				if (!searchActive) {
-					searchForm.addClass('active');
-					searchActive = true;
-
-					$(document).one('click', function closeForm(e) {
-						if ($(e.target).hasClass('search_content_input')) {
-							$(document).one('click', closeForm);
-						}
-						else {
-							searchForm.removeClass('active');
-							searchActive = false;
-						}
-					});
-				}
-				else {
-					searchForm.removeClass('active');
-					searchActive = false;
-				}
-			});
-		}
 	}
 });
