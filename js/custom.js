16e5a08ffb604aaf874621c5b38f3922f78910a0
NamTang <namkty96@gmail.com>
2018-09-07 18:32:23 +0700

    add animate for images

--- js/custom.js
+++ js/custom.js
@@ -24,7 +24,9 @@ $(document).ready(function () {
 	var sideMenu = $('.hamburger-side-menu');
 	var header = $('.header');
 	var imgIndex = 0;
-	var images = ["./images/cert-java.jpg", "./images/cert-html.jpg", "./images/cert-css.jpg", "./images/cert-js.jpg", "./images/cert-jquery.jpg", "./images/cert-php.jpg", "./images/cert-sql.jpg"];
+	var imgPrevIndex;
+
+	//var images = ["./images/cert-java.jpg", "./images/cert-html.jpg", "./images/cert-css.jpg", "./images/cert-js.jpg", "./images/cert-jquery.jpg", "./images/cert-php.jpg", "./images/cert-sql.jpg"];
 
 	$('a[href^="#"]').on('click', function (event) {
 		event.preventDefault();
@@ -102,30 +104,46 @@ $(document).ready(function () {
 		if ($('.home_slider_prev').length) {
 
 			$('.home_slider_prev').on('click', function () {
+				imgPrevIndex = imgIndex;
 				imgIndex -= 1;
 
 				if (imgIndex < 0) {
-					imgIndex = images.length - 1;
+					imgIndex = 7 - 1;
 				}
 
-				$('img.center-fit').attr('src', images[imgIndex]);
-				$('.item-count').html((imgIndex + 1) + '/' + images.length);
+				imageAnimate(imgPrevIndex, imgIndex);
 			});
 		}
 
 		if ($('.home_slider_next').length) {
 
 			$('.home_slider_next').on('click', function () {
+				imgPrevIndex = imgIndex;
 				imgIndex += 1;
 
-				if (imgIndex >= images.length) {
+				if (imgIndex >= 7) {
 					imgIndex = 0;
 				}
 
-				$('img.center-fit').attr('src', images[imgIndex]);
-				$('.item-count').html((imgIndex + 1) + '/' + images.length);
+				imageAnimate(imgPrevIndex, imgIndex);
 			});
 		}
+
+	}
+
+	function imageAnimate(imgPrevIndex, imgIndex) {
+
+		$($('img.center-fit')[imgPrevIndex]).animate({
+			opacity: 0.25,
+			left: "+=150px",
+		  }, 1000, function() {
+			$($('img.center-fit')[imgPrevIndex]).css("display", "none");
+			$($('img.center-fit')[imgPrevIndex]).css("opacity", "");
+			$($('img.center-fit')[imgIndex]).css("display", "");
+			$('.item-count').html((imgIndex + 1) + '/' + 7);
+		  });
+
+		//$('img.center-fit').attr('src', images[imgIndex]);
 
 	}
 
